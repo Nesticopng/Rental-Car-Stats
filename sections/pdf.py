7014fde94ca8c0df48658b31b266a7fda4c38af3
Ronald Maneiro <romaneiro2@gmail.com>
2025-03-13 11:14:55 -0400

    Agregar boton en Stremlit para generar y descagar PDF - Agregado boton de generacion y descargar del PDf en el archivo pdf.py

--- sections/pdf.py
+++ sections/pdf.py
@@ -1,6 +1,22 @@
 import streamlit as st
+from src.utils.generar_pdf import generar_pdf
 
 
 def PDF():
     st.title("📄 PDF")
-    st.write("Aquí podras ver el PDF automatizado.")
+
+    if st.button("Generar PDF", type="primary"):
+        pdf_path = generar_pdf()
+        st.success("✅ PDF generado con éxito.")
+
+    # Leer el archivo generado para que el usuario lo descargue
+        with open(pdf_path, "rb") as pdf_file:
+            pdf_bytes = pdf_file.read()
+
+        # Botón de descarga
+        st.download_button(
+            label="📥 Descargar PDF",
+            data=pdf_bytes,
+            file_name="Trabajo_final.pdf",
+            mime="application/pdf"
+        )
